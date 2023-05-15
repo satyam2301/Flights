9cfea20cc69049177f8591bfbede853b3debcbaa
satyam2301 <satyamj.2301@gmail.com>
2023-05-15 23:48:09 +0530

    added new migration file which define foreign key relation between city and airports

---
+++ src/migrations/20230515172549-update-city-airport-association.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addConstraint('Airports', {
+      fields: ['cityId'],
+      type: 'FOREIGN KEY',
+      name: 'city_fk_constraints',
+      references: {
+        table: 'Cities',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeConstraint('Airports', 'city_fk_constraints');
+  },
+};
--- src/models/airport.js
+++ src/models/airport.js
@@ -1,7 +1,7 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class City extends Model {
+  class Airport extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -9,23 +9,38 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.hasMany(models.Airport, {
-        foreignKey: 'cityId',
+      this.belongsTo(models.City, {
+        foreignKey: 'city_id',
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
       });
     }
   }
-  City.init(
+  Airport.init(
     {
       name: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
       },
+      code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      address: {
+        type: DataTypes.STRING,
+        unique: true,
+      },
+      cityId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
     },
     {
       sequelize,
-      modelName: 'City',
+      modelName: 'Airport',
     }
   );
-  return City;
+  return Airport;
 };
