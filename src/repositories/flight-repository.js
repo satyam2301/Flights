8d1276a1967744729378b3fc4e8ab97a5e342e98
satyam2301 <satyamj.2301@gmail.com>
2023-06-09 15:32:32 +0530

    FIX: added raw query to separte file

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -2,6 +2,7 @@ const CrudRepository = require('./crud-repository');
 const { Flight, Airplane, Airport, City } = require('../models');
 const { Sequelize } = require('sequelize');
 const db = require('../models');
+const { addRowLockOnFlights } = require('./queries');
 
 class FlightRepository extends CrudRepository {
   constructor() {
@@ -65,9 +66,7 @@ class FlightRepository extends CrudRepository {
   }
 
   async updateRemainingSeats(flightId, seats, dec = true) {
-    await db.sequelize.query(
-      `SELECT * FROM Flights WHERE Flights.id = ${flightId} FOR UPDATE;`
-    );
+    await db.sequelize.query(addRowLockOnFlights(flightId));
     const flight = await Flight.findByPk(flightId);
     if (parseInt(dec)) {
       await flight.decrement('totalSeats', { by: seats });
